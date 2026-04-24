7842f88d93235f93a029faba19e121a35ac9fce1
Allan da Silva <allanweb019@gmail.com>
2026-04-23 21:56:23 -0300

    new site

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import "./App.css";
 
 function App() {
-  return <h1>Ola, Mundo! meu nome é Allan</h1>;
+  return (
+    <div className="app">
+      <h1>Ola, Mundo! meu nome é Allan</h1>
+    </div>
+  );
 }
 
 export default App;
